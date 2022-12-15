d220d2962d0b0bbf99f3e53564f0dfd861239bf4
sword4869 <1084662708@qq.com>
2022-12-15 09:34:24 +0800

    vuepress2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,22 +1,18 @@
-import { defineConfig } from "vuepress/config";
-export default defineConfig({
-  title: "Hello sword4869/vuepress-starter",
-  base: "/vuepress-starter/",
-  plugins: [
-    "vuepress-plugin-helper-live2d",
-    "vuepress-plugin-reading-progress",
-    "vuepress-plugin-code-copy",
-  ],
-  themeConfig: {
-    docsRepo: "sword4869/vuepress-starter",
-    docsBranch: "main",
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  title: "Hello",
+  theme: hopeTheme({
+    repo: "sword4869/sword4869.github.io",
     docsDir: "docs",
-    editLinks: true,
-    editLinkText: "Help us improve this page!",
     sidebar: {
       "/": [
-        ["/", "README"]
+        {
+          text: "README",
+          link: "/",
+        }
       ],
     },
-  },
+  }),
 });
