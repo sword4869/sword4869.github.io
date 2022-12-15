f860d80fed21abdae0c194ed524a21a2d4c73eb8
sword4869 <1084662708@qq.com>
2022-12-15 10:23:20 +0800

    base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { hopeTheme } from "vuepress-theme-hope";
 
 export default defineUserConfig({
   title: "Hello",
+  base: "/",
   theme: hopeTheme({
     repo: "sword4869/sword4869.github.io",
     docsDir: "docs",
